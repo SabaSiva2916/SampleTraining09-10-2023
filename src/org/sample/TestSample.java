f02111132c394bb244ddcf63d15ad7b415d9859e
SabaSiva2916 <sabasiva2916@gmail.com>
2023-10-11 19:34:30 +0530

    production changes

--- src/org/sample/TestSample.java
+++ src/org/sample/TestSample.java
@@ -1,8 +1,27 @@
 package org.sample;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class TestSample {
 
     public static void main(String[] args) {
 	System.out.println("Hello World");
+	// creating list adding String values
+	List<String> li = new ArrayList<>();
+	li.add("java");
+	li.add("Selenium");
+	li.add("Python");
+	li.add("Api Testing");
+
+	Iterator<String> iterator = li.iterator();
+	while (iterator.hasNext()) {
+	    String string = iterator.next();
+	    System.out.println(string);
+	}
+	
+	
     }
+
 }
